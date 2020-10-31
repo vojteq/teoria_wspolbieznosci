50a1354831b1791edfcdc3c7f178ca216563df05
vojteq <vojteq17@gmail.com>
2020-10-31 14:47:21 +0100

    lab5 done?

--- src/lab5/Monitor.java
+++ src/lab5/Monitor.java
@@ -2,6 +2,7 @@ package lab5;
 
 import javax.lang.model.type.NullType;
 import java.lang.reflect.Array;
+import java.util.LinkedHashMap;
 import java.util.LinkedList;
 import java.util.Random;
 import java.util.concurrent.locks.Condition;
@@ -10,11 +11,19 @@ import java.util.concurrent.locks.ReentrantLock;
 public class Monitor {
     // TODO: jaka struktura do values zeby sie dalo sprawdzic jej rozmiar ale zeby ZAPIS ODCZYT USUNIECIE byly O(1)
     // TODO: wyjebac tablice ...taken?
-    private int[] values;
+    // TODO: ktore signal() gdzie
+
+    // klucz -> id producenta
+    // wartpsc -> wyprodukowana liczba
+    private LinkedHashMap<Integer, Integer> values;
     private LinkedList<ProducerTicket> producerTickets;
-    private LinkedList<ConsumerTicket> consumerTickets;
-    private ProducerTicket[] producerTicketsTaken;
-    private ConsumerTicket[] consumerTicketsTaken;
+
+    // klucz -> id producenta ktory wyprodukowal a wiec stworzyl ConsumerTicket
+    // wartosc ->
+    private LinkedHashMap<Integer, ConsumerTicket> consumerTickets;
+
+    // id producenta ktory wyprodukowal, po zjedzeniu przez konsumenta usuwamy jedna wartosc
+    private LinkedList<Integer> producedBy;
     private int maxSize;
     private Random random;
     private ReentrantLock producerTicketLock;
@@ -22,18 +31,14 @@ public class Monitor {
     private Condition producersCond;
     private Condition consumersCond;
 
+
     public Monitor(int maxSize) {
-        values = new int[maxSize];
+        values = new LinkedHashMap<>();
         producerTickets = new LinkedList<>();
-        consumerTickets = new LinkedList<>();
-        producerTicketsTaken = new ProducerTicket[maxSize];
-        consumerTicketsTaken = new ConsumerTicket[maxSize];
+        consumerTickets = new LinkedHashMap<>();
+        producedBy = new LinkedList<>();
         for (int i = 0; i < maxSize; i++) {
-            values[i] = -1;
             producerTickets.add(new ProducerTicket(i));
-//            consumerTickets.add(new ConsumerTicket(i));
-            producerTicketsTaken[i] = null;
-            consumerTicketsTaken[i] = null;
         }
         random = new Random();
         this.maxSize = maxSize;
@@ -47,13 +52,12 @@ public class Monitor {
         ProducerTicket ticket = null;
         producerTicketLock.lock();
         try {
-            while (producerTickets.size() == 0) {
-                System.out.println("producer is waiting");
+            while (values.size() >= maxSize || producerTickets.size() == 0) {
+                System.out.println("producer " + Thread.currentThread().getId() + " is waiting");
                 producersCond.await();
             }
             ticket = producerTickets.get(0);
             producerTickets.remove(0);
-            producerTicketsTaken[ticket.getId()] = ticket;
             producersCond.signal();
         }
         catch (InterruptedException e) {
@@ -70,12 +74,11 @@ public class Monitor {
         consumerTicketLock.lock();
         try {
             while (consumerTickets.size() == 0) {
-                System.out.println("producer is waiting");
+                System.out.println("consumer " + Thread.currentThread().getId() + " is waiting");
                 consumersCond.await();
             }
             ticket = consumerTickets.get(0);
-            producerTickets.remove(0);
-            consumerTicketsTaken[ticket.getId()] = ticket;
+            consumerTickets.remove(producedBy.remove(0));
             consumersCond.signal();
         }
         catch (InterruptedException e) {
@@ -88,25 +91,15 @@ public class Monitor {
     }
 
     public void produce(ProducerTicket ticket) {
-        if (ticket.equals(producerTicketsTaken[ticket.getId()])) {
-            values[ticket.getId()] = (random.nextInt() % 50) + 50;
-            System.out.println("produced: " + values[ticket.getId()]);
-            producerTickets.add(ticket);
-        }
-        else {
-            System.out.println("producer ticket invalid");
-        }
+        int producedValue = random.nextInt() % 50 + 50;
+        values.put(ticket.getId(), producedValue);
+        producedBy.add(ticket.getId());
+        System.out.println("produced: " + producedValue);
+        producerTickets.add(ticket);
+        consumerTickets.put(ticket.getId(), new ConsumerTicket(ticket.getId()));
     }
 
     public void consume(ConsumerTicket ticket) {
-        if (ticket.equals(consumerTicketsTaken[ticket.getId()])) {
-            System.out.println("consumed: " + values[ticket.getId()]);
-            values[ticket.getId()] = -1;
-            consumerTickets.add(ticket);
-            consumerTicketsTaken[ticket.getId()] = null;
-        }
-        else {
-            System.out.println("consumer ticket invalid");
-        }
+        System.out.println("consumed: " + values.remove(ticket.getId()));
     }
 }
